0daf47a9e9f819dd47a621f24ad3c607406f1d0f
segersniels <segers.n@hotmail.com>
2018-02-14 17:42:49 +0100

    refactored large part of the exec code ; version 0.1.8

--- index.js
+++ index.js
@@ -17,56 +17,104 @@ program
     .command('stop')
     .description('Stop a running container')
     .action(() => {
-        if (typeof process.argv[3] === 'undefined') exec.stopContainer();
-        else exec.docker();
+        if (typeof process.argv[3] === 'undefined') {
+            const question = 'Which container would you like to stop?';
+            const error = 'No containers available to stop';
+            exec.executeCommand('ps', question, 'stop', error);
+        } else {
+            exec.docker();
+        }
     });
 
 program
     .command('start')
     .description('Start a stopped container')
     .action(() => {
-        if (typeof process.argv[3] === 'undefined') exec.startContainer();
-        else exec.docker();
+        if (typeof process.argv[3] === 'undefined') {
+            const question = 'Which container would you like to start?';
+            const error = 'No containers available to start';
+            exec.executeCommand('psaStopped', question, 'start', error);
+        } else {
+            exec.docker();
+        }
     });
 
 program
     .command('logs')
     .description('See the logs of a container')
     .action(() => {
-        if (typeof process.argv[3] === 'undefined') exec.logContainer();
-        else exec.docker();
+        if (typeof process.argv[3] === 'undefined') {
+            const question = 'Which container would you like to see the logs of?';
+            const error = 'No containers to see the logs of';
+            exec.executeCommand('psa', question, 'logs', error);
+        } else {
+            exec.docker();
+        }
     });
 
 program
     .command('rm')
     .description('Remove a container')
     .action(() => {
-        if (typeof process.argv[3] === 'undefined') exec.removeContainer();
-        else exec.docker();
+        if (typeof process.argv[3] === 'undefined') {
+            const question = 'Which container would you like to remove?';
+            const error = 'No containers to remove';
+            exec.executeCommand('psa', question, 'rm', error);
+        } else {
+            exec.docker();
+        }
     });
 
 program
     .command('rmi')
     .description('Remove an image')
     .action(() => {
-        if (typeof process.argv[3] === 'undefined') exec.removeImage();
-        else exec.docker();
+        if (typeof process.argv[3] === 'undefined') {
+            const question = 'Which image would you like to remove?';
+            const error = 'No images to remove';
+            exec.executeCommand('images', question, 'rmi', error);
+        } else {
+            exec.docker();
+        }
     });
 
 program
     .command('stats')
     .description('See the stats of a container')
     .action(() => {
-        if (typeof process.argv[3] === 'undefined') exec.statsContainer();
-        else exec.docker();
+        if (typeof process.argv[3] === 'undefined') {
+            const question = 'Which containers would you like to see that stats of?';
+            const error = 'No containers available';
+            exec.executeCommand('ps', question, 'stats', error);
+        } else {
+            exec.docker();
+        }
     });
 
 program
     .command('ssh')
     .description('SSH into a container')
     .action(() => {
-        if (typeof process.argv[3] === 'undefined') exec.sshContainer();
-        else exec.docker();
+        if (typeof process.argv[3] === 'undefined') {
+            const question = 'Which container would you like to SSH to?';
+            const error = 'No containers available';
+            exec.executeCommand('ps', question, 'ssh', error);
+        } else {
+            exec.docker();
+        }
+    });
+
+program
+    .command('history')
+    .description('See the history of an image')
+    .action(() => {
+        if (typeof process.argv[3] === 'undefined') {
+            const question = 'Which image would you like to see the history of?';
+            const error = 'No images available';
+            exec.executeCommand('images', question, 'history', error);
+        } else {
+            exec.docker();
+        }
     });
 
 program
