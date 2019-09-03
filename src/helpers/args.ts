0ee69b84175783033b27904d6c79b585d0ebc060
segersniels <segers.n@hotmail.com>
2019-09-03 16:36:33 +0200

    :ambulance: Make sure we parse flags correctly :hankey:

--- src/helpers/args.ts
+++ src/helpers/args.ts
@@ -1,5 +1,9 @@
 const argv = require('minimist')(process.argv.slice(2))
 
+const nonFlags: any = {}
+// TODO: Add a way to metadata so we can skip it from there instead of harcoding
+const CONFIRMED_BOOLEAN_FLAGS = ['f', 'follow']
+
 const getFlagArguments = () => {
   const keys = Object.keys(argv)
   const flags: any = {}
@@ -7,13 +11,17 @@ const getFlagArguments = () => {
     if (key === '_') {
       continue
     }
-    flags[key] = argv[key]
+    if (CONFIRMED_BOOLEAN_FLAGS.includes(key)) {
+      nonFlags[argv[key]] = true
+      flags[key] = true
+    } else {
+      flags[key] = argv[key]
+    }
   }
   return flags
 }
 
 const getNonFlagArguments = () => {
-  const nonFlags: any = {}
   for (const key of argv._.slice(1, argv._.length)) {
     nonFlags[key] = true
   }
