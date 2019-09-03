bb73dcddfd50115627ef88b0db86c480007bebad
segersniels <segers.n@hotmail.com>
2019-09-03 17:31:04 +0200

    :ambulance: Fuck my life fix the rc and bump

--- src/helpers/args.ts
+++ src/helpers/args.ts
@@ -13,7 +13,7 @@ const getFlagArguments = () => {
       continue
     }
     // Make sure we correctly parse custom flags, for now these are boolean flags so parse them correctly
-    if (metadata[command].flags) {
+    if (command && metadata[command].flags) {
       const commandFlags = flatten(metadata[command].flags)
       if (commandFlags.includes(key)) {
         nonFlags.push(argv[key])
