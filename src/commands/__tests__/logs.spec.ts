361063b0ea6c9018e1e2b3a31aa8d69153863b5c
segersniels <segers.n@hotmail.com>
2020-02-29 17:19:25 +0100

    :white_check_mark: Test if short logs config changes the flags

--- src/commands/__tests__/logs.spec.ts
+++ src/commands/__tests__/logs.spec.ts
@@ -1,8 +1,15 @@
 import 'mocha';
 import Logs from 'commands/logs';
+import Config from 'commands/config';
 import { expect } from 'chai';
+import ConfigOptions from 'enums/ConfigOptions';
 
-describe('logs', () => {
+describe('logs', async () => {
+  const config = new Config('disable', {
+    prompt: () => ({ choice: ConfigOptions.SHORT_LOGS }),
+  });
+  await config.run();
+
   it('should correctly execute', async () => {
     const command = new Logs({
       createChoices: () => ['123 - abc', '456 - foo', '789 - bar'],
@@ -18,5 +25,19 @@ describe('logs', () => {
       parseFlags: () => ['--follow'],
     });
     expect(await command.run()).to.eql('docker logs --follow 456');
+  });
+
+  it('should append short logs flags', async () => {
+    const config = new Config('enable', {
+      prompt: () => ({ choice: ConfigOptions.SHORT_LOGS }),
+    });
+    await config.run();
+
+    const command = new Logs({
+      createChoices: () => ['123 - abc', '456 - foo', '789 - bar'],
+      determineChoice: () => '456 - foo',
+      parseFlags: () => ['--follow'],
+    });
+    expect(await command.run()).to.eql('docker logs --tail 500 --follow 456');
   });
 });
