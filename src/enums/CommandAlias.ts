854f4fb7c028ebf75509a26676caf2f270ed2baa
segersniels <segers.n@hotmail.com>
2020-03-06 11:20:10 +0100

    :sparkles: Also show image name in prompt in case container has an automated name

--- src/enums/CommandAlias.ts
+++ src/enums/CommandAlias.ts
@@ -1,7 +1,7 @@
 const enum CommandAlias {
-  RUNNING_CONTAINERS = 'docker ps --format "{{.ID}} - {{.Names}}"',
-  ALL_CONTAINERS = 'docker ps --format "{{.ID}} - {{.Names}}" --all',
-  STOPPED_CONTAINERS = 'docker ps --format "{{.ID}} - {{.Names}}" --all --filter status=exited',
+  RUNNING_CONTAINERS = 'docker ps --format "{{.ID}} - {{.Names}} ({{.Image}})"',
+  ALL_CONTAINERS = 'docker ps --format "{{.ID}} - {{.Names}} ({{.Image}})" --all',
+  STOPPED_CONTAINERS = 'docker ps --format "{{.ID}} - {{.Names}} ({{.Image}})" --all --filter status=exited',
   IMAGES = 'docker images --format "{{.ID}} - {{.Repository}}"',
 }
 
