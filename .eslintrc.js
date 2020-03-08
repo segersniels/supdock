3d255cff39fd4525a4cd6b7a290c4f92e7368ff4
segersniels <segers.n@hotmail.com>
2020-03-08 12:28:44 +0100

    :fire: Remove react from eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,16 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
-    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
-  plugins: ['react-hooks'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-inferrable-types': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
     '@typescript-eslint/no-unused-vars': 'error',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
   },
 };
