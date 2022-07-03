ddc545062dc50092347cb867aae9692f5121dc82
segersniels <segers.n@hotmail.com>
2022-07-04 01:51:47 +0200

    :recycle: Check for identical matches and not on partial split() matches

--- src/helpers/fuzzy.ts
+++ src/helpers/fuzzy.ts
@@ -16,63 +16,63 @@ export default class Fuzzy {
     const term = that.args!.nonFlags[0];
     const choicesAfterFuzzySearching = fuzzy(choices, term);
 
-    if (choicesAfterFuzzySearching.length === 0) {
-      throw new ExecutionError(
-        `Was not able to match with container or image for search: ${term}`,
-      );
-    }
-
-    // Identical match found based on id or name
-    if (choicesAfterFuzzySearching.length === 1) {
-      const choice = choicesAfterFuzzySearching[0];
-
-      if (
-        (term === choice.split('-')[0].trim() ||
-          term === choice.split('-')[1].trim() ||
-          choice
-            .split('-')[0]
-            .trim()
-            .startsWith(term)) &&
-        !info.custom // Don't default custom commands
-      ) {
-        return that.default!();
-      }
-
-      // Ask the user for confirmation
-      if (config.get(ConfigOptions.CAUTION_CHECK)) {
-        UtilHelper.info(
-          `You can disable this check by running 'supdock disable caution-check'`,
-          true,
+    switch (choicesAfterFuzzySearching.length) {
+      case 0: {
+        throw new ExecutionError(
+          `Was not able to match with container or image for search: ${term}`,
         );
-
-        const confirmation = await prompts({
-          type: 'confirm',
-          name: 'choice',
-          message: `Are you sure you want to execute '${that.command}' for container '${choice}'`,
-          initial: true,
-        });
-
-        if (!confirmation.choice) {
-          throw new ExecutionError('Exiting on request of user...');
-        }
       }
+      // Identical match found based on id or name
+      case 1: {
+        const choice = choicesAfterFuzzySearching[0];
 
-      return choice;
-    }
-
-    if (
-      choicesAfterFuzzySearching.find(
-        choice =>
+        if (
           (term === choice.split('-')[0].trim() ||
             term === choice.split('-')[1].trim() ||
             choice
               .split('-')[0]
               .trim()
               .startsWith(term)) &&
-          !info.custom,
-      )
-    ) {
-      return that.default!();
+          !info.custom // Don't default custom commands
+        ) {
+          return that.default!();
+        }
+
+        // Ask the user for confirmation
+        if (config.get(ConfigOptions.CAUTION_CHECK)) {
+          UtilHelper.info(
+            `You can disable this check by running 'supdock disable caution-check'`,
+            true,
+          );
+
+          const confirmation = await prompts({
+            type: 'confirm',
+            name: 'choice',
+            message: `Are you sure you want to execute '${that.command}' for container '${choice}'`,
+            initial: true,
+          });
+
+          if (!confirmation.choice) {
+            throw new ExecutionError('Exiting on request of user...');
+          }
+        }
+
+        return choice;
+      }
+    }
+
+    for (const choice of choicesAfterFuzzySearching) {
+      const [id, name] = choice.split('-', 2);
+
+      // Check if remaining options have an identical match on container id
+      if ((term === id.trim() || id.trim().startsWith(term)) && !info.custom) {
+        return that.default!();
+      }
+
+      // Check for identical match on name
+      if (term === name.trim()) {
+        return that.default!();
+      }
     }
 
     const { choice } = await prompts({
