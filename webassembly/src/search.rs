d4bb4cda3ce90663b3055d7720b8c987dbfb8cab
segersniels <segers.n@hotmail.com>
2022-07-04 13:31:54 +0200

    :recycle: Implement own jaro_winkler calculation to have more control over the fuzzy searching itself

--- webassembly/src/search.rs
+++ webassembly/src/search.rs
@@ -1,19 +1,47 @@
-use simsearch::{SearchOptions, SimSearch};
+use std::collections::HashSet;
+use strsim::jaro_winkler;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: JsValue);
+}
+
+fn parse(needle: String) -> Vec<String> {
+    let mut words: Vec<String> = needle
+        .split_whitespace()
+        .map(|token| token.to_string().to_lowercase())
+        .collect();
+
+    // Filter out empty
+    words.retain(|word| !word.is_empty());
+
+    // Deduplicate words, we don't benefit from matching duplicates
+    words.dedup();
+
+    return words;
+}
 
 pub fn search(haystack: Vec<String>, needle: String, threshold: f64) -> Vec<String> {
-    let mut engine: SimSearch<u32> = SimSearch::new_with(SearchOptions::new().threshold(threshold));
+    let mut results = HashSet::new();
 
-    // Insert the potential "needles" into the engine
-    for (index, potential_needle) in haystack.iter().enumerate() {
-        engine.insert(index.try_into().unwrap(), potential_needle);
+    for potential_needle in haystack.iter() {
+        let words = parse(potential_needle.to_owned());
+
+        for word in words {
+            let distance = jaro_winkler(&word.to_lowercase(), &needle.to_lowercase());
+
+            // Not similar enough
+            if distance < threshold {
+                continue;
+            }
+
+            // Found similarity that exceeds our threshold, stop looking
+            results.insert(potential_needle.to_owned());
+            break;
+        }
     }
 
-    // Perform the search
-    let results: Vec<u32> = engine.search(&needle);
-    let results = results
-        .iter()
-        .map(|index| haystack[index.to_owned() as usize].to_string())
-        .collect::<Vec<_>>();
-
-    return results;
+    return Vec::from_iter(results);
 }
