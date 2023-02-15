2a3ac31c938f454f1e297679ef52b646bafca768
segersniels <segers.n@hotmail.com>
2023-02-15 15:58:10 +0100

    :hammer: Make sure we generate missing tags if they don't exist yet

---
+++ scripts/generate-tags.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+git log --oneline | grep :bookmark: |
+  while read line; do
+    version=$(echo $line | awk '{print $3}')
+    hash=$(echo $line | awk '{print $1}')
+    git tag -a $version $hash -m "${version}" 2>/dev/null
+  done
+
+git push --follow-tags
