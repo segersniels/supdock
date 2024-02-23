6a4789b28328b31ad216f4919b9e4bbabec24e3e
segersniels <segers.n@hotmail.com>
2024-02-23 10:36:29 +0100

    :recycle: Search the haystack in parallel

--- src/utils/search.rs
+++ src/utils/search.rs
@@ -1,4 +1,9 @@
-use std::{collections::HashSet, slice};
+use std::{
+    collections::HashSet,
+    slice,
+    sync::{Arc, Mutex},
+    thread,
+};
 use strsim::jaro_winkler;
 
 /// Parse a needle string into a vector of lowercase words, filtered for empty strings and duplicates.
@@ -23,39 +28,50 @@ fn is_similar(word: &str, needle: &str, threshold: f64) -> bool {
 }
 
 /// Search for a needle in a haystack
-pub fn search<T, I>(haystack: I, needle: &str, threshold: f64, split_char: &str) -> Vec<String>
-where
-    T: AsRef<str>,
-    I: IntoIterator<Item = T>,
-{
-    let mut results = HashSet::new();
+pub fn search(haystack: &[String], needle: &str, threshold: f64, split_char: &str) -> Vec<String> {
+    let mut threads = Vec::new();
+    let results: Arc<Mutex<HashSet<String>>> = Arc::new(Mutex::new(HashSet::new()));
 
-    // Look for similarity in each of the options
-    for potential_needle in haystack.into_iter() {
-        let words = parse(potential_needle.as_ref(), split_char);
+    for potential_needle in haystack.iter() {
+        let results = Arc::clone(&results);
+        let potential_needle = potential_needle.to_owned();
+        let needle = needle.to_owned();
+        let split_char = split_char.to_owned();
 
-        // Crawl word by word to see if we can find similarity
-        for word in &words {
-            // Similarity already found, stop looking
-            if is_similar(word, needle, threshold) {
-                results.insert(potential_needle.as_ref().to_owned());
+        threads.push(thread::spawn(move || {
+            let words = parse(&potential_needle, &split_char);
+
+            // Crawl word by word to see if we can find similarity
+            for word in words {
+                let mut results = results.lock().unwrap();
+
+                // Similarity already found, stop looking
+                if is_similar(&word, &needle, threshold) {
+                    results.insert(potential_needle);
+                    break;
+                }
+
+                if !word.contains('-') {
+                    continue;
+                }
+
+                // Check for similarity split by dashes in case of name (eg. this-is-my-container-name)
+                if search(slice::from_ref(&word), &needle, threshold, "-").is_empty() {
+                    continue;
+                }
+
+                // Found similarity after splitting by dashes, stop looking
+                results.insert(potential_needle);
                 break;
             }
-
-            if !word.contains('-') {
-                continue;
-            }
-
-            // Check for similarity split by dashes in case of name (eg. this-is-my-container-name)
-            if search(slice::from_ref(word), needle, threshold, "-").is_empty() {
-                continue;
-            }
-
-            // Found similarity after splitting by dashes, stop looking
-            results.insert(potential_needle.as_ref().to_owned());
-            break;
-        }
+        }));
     }
 
-    results.into_iter().collect()
+    // Wait for threads to complete
+    for thread in threads {
+        thread.join().unwrap();
+    }
+
+    let result = results.lock().unwrap();
+    result.iter().cloned().collect()
 }
